feac085d7a6977101f7507af5e0a393d610c2ad9
Ivan Kravets <me@ikravets.com>
2014-05-18 23:41:07 +0300

    Added Embedded-Blink example (works with Wiring based frameworks, like Arduino or Energia)

---
+++ examples/embedded-blink/src/blink.cpp
@@ -0,0 +1,29 @@
+/*
+  Turns ON and OFF the Wiring compatible board LED, with intervals of 1 second (1000 milliseconds)
+
+ */
+
+#ifdef ENERGIA
+
+#include "Energia.h"
+#define WLED  RED_LED
+
+#else
+
+#include "Arduino.h"
+#define WLED	13  // Most Arduino boards already have an LED attached to pin 13 on the board itself
+
+#endif
+
+void setup()
+{
+  pinMode(WLED, OUTPUT);  // set pin as output
+}
+
+void loop()
+{
+  digitalWrite(WLED, HIGH);  // set the LED on
+  delay(1000);               // wait for a second
+  digitalWrite(WLED, LOW);   // set the LED off
+  delay(1000);               // wait for a second
+}
