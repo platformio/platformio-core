05908dc882e04c97a7f7df2237909f510b587f82
Ivan Kravets <me@ikravets.com>
2014-08-04 12:09:04 +0300

    Continue on 1.0.0-dev

--- platformio/__init__.py
+++ platformio/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (C) Ivan Kravets <me@ikravets.com>
 # See LICENSE for details.
 
-VERSION = (0, 5, 0)
+VERSION = (1, 0, "0-dev")
 __version__ = ".".join([str(s) for s in VERSION])
 
 __title__ = "platformio"
