70493a7721cac6e48a722b51e30338215fecf647
Ivan Kravets <me@ikravets.com>
2014-09-12 19:17:31 +0300

    Update PlatformIO Website URLs

--- platformio/__init__.py
+++ platformio/__init__.py
@@ -1,12 +1,12 @@
 # Copyright (C) Ivan Kravets <me@ikravets.com>
 # See LICENSE for details.
 
-VERSION = (0, 7, "0.dev")
+VERSION = (0, 7, "0-dev")
 __version__ = ".".join([str(s) for s in VERSION])
 
 __title__ = "platformio"
 __description__ = ("A cross-platform code builder and library manager")
-__url__ = "https://github.com/ivankravets/platformio"
+__url__ = "http://platformio.ikravets.com"
 
 __author__ = "Ivan Kravets"
 __email__ = "me@ikravets.com"
@@ -14,6 +14,5 @@ __email__ = "me@ikravets.com"
 __license__ = "MIT License"
 __copyright__ = "Copyright (C) 2014 Ivan Kravets"
 
-# __apiurl__ = "http://127.0.0.1:8080"
 __apiurl__ = "http://api.platformio.ikravets.com"
 __pkgmanifesturl__ = "http://dl.platformio.ikravets.com/packages/manifest.json"
