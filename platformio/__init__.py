01d415cc7f89dea8b10190e90d729364cff3bdb7
Ivan Kravets <me@ikravets.com>
2014-10-15 22:33:23 +0300

    Avoid trademarks issues

--- platformio/__init__.py
+++ platformio/__init__.py
@@ -5,8 +5,8 @@ VERSION = (0, 8, "0-dev")
 __version__ = ".".join([str(s) for s in VERSION])
 
 __title__ = "platformio"
-__description__ = ("A cross-platform code builder and library manager "
-                   "(Arduino, MSP430, ARM)")
+__description__ = ("A cross-platform code builder and library manager for "
+                   "Arduino, MSP430, ARM")
 __url__ = "http://platformio.ikravets.com"
 
 __author__ = "Ivan Kravets"
