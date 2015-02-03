423dc248bc7ba7bab6f561a37e685784afd60c31
Valeriy Koval <valeros@bigmir.net>
2015-02-03 14:43:27 +0200

    Reorganize examples with STM32 and new frameworks

---
+++ examples/stm32/stm32-cmsis-blink/src/main.c
@@ -0,0 +1,27 @@
+#include "stm32l1xx.h"
+
+void ms_delay(int ms)
+{
+   while (ms-- > 0) {
+      volatile int x=500;
+      while (x-- > 0)
+         __asm("nop");
+   }
+}
+
+
+//Alternates blue and green LEDs quickly
+int main(void)
+{
+    RCC->AHBENR |= RCC_AHBENR_GPIOBEN; 		 // enable the clock to GPIOB
+    GPIOB->MODER |= (1 << 12);             // set pin 6 to be general purpose output
+    GPIOB->MODER |= (1 << 14);             // set pin 7 to be general purpose output
+
+    GPIOB->ODR |= (1 << 7);           			// Set the pin 7 high
+
+    for (;;) {
+       ms_delay(100);
+       GPIOB->ODR ^= (1 << 6);           // Toggle the pin 
+       GPIOB->ODR ^= (1 << 7);           // Toggle the pin 
+    }
+}
---
+++ examples/stm32/stm32-spl-blink/src/main.c
@@ -0,0 +1,49 @@
+#include <stm32l1xx_gpio.h>
+#include <stm32l1xx_rcc.h>
+#include <misc.h>
+
+/* timing is not guaranteed :) */
+void simple_delay(uint32_t us)
+{
+	/* simple delay loop */
+	while (us--) {
+		asm volatile ("nop");
+	}
+}
+
+/* system entry point */
+int main(void)
+{
+	/* gpio init struct */
+	GPIO_InitTypeDef gpio;
+
+	/* reset rcc */
+	RCC_DeInit();
+
+	/* enable clock to GPIOB */
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
+
+	/* use pin 7 */
+	gpio.GPIO_Pin = GPIO_Pin_7;
+	/* mode: output */
+	gpio.GPIO_Mode = GPIO_Mode_OUT;
+	/* output type: push-pull */
+	gpio.GPIO_OType = GPIO_OType_PP;
+	/* apply configuration */
+	GPIO_Init(GPIOB, &gpio);
+
+	/* main program loop */
+	for (;;) {
+		/* set led on */
+		GPIO_SetBits(GPIOB, GPIO_Pin_7);
+		/* delay */
+		simple_delay(100000);
+		/* clear led */
+		GPIO_ResetBits(GPIOB, GPIO_Pin_7);
+		/* delay */
+		simple_delay(100000);
+	}
+
+	/* never reached */
+	return 0;
+}
