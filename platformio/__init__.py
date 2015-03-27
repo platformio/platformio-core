5a0d57f50718ebbe02f9d67d2e435fe5f365b56e
Ivan Kravets <me@ikravets.com>
2015-03-27 17:04:09 +0200

    Version bump to 1.3.0 (issues #127 #128 #130 #131 #133 #136 #137 #138 #139 #140)

--- platformio/__init__.py
+++ platformio/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (C) Ivan Kravets <me@ikravets.com>
 # See LICENSE for details.
 
-VERSION = (1, 3, "0.dev1")
+VERSION = (1, 3, 0)
 __version__ = ".".join([str(s) for s in VERSION])
 
 __title__ = "platformio"
