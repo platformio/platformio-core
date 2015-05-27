ca1df9462dd91b342afb4edd0b9c5f830823b8f7
Ivan Kravets <me@ikravets.com>
2015-05-27 19:32:47 +0300

    Bump version to 2.0.2

--- platformio/__init__.py
+++ platformio/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (C) Ivan Kravets <me@ikravets.com>
 # See LICENSE for details.
 
-VERSION = (2, 0, "2.dev0")
+VERSION = (2, 0, 2)
 __version__ = ".".join([str(s) for s in VERSION])
 
 __title__ = "platformio"
