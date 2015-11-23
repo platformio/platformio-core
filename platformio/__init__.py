c0eded84a09e1a9c9dbd8e45427717b2a357bf3d
Ivan Kravets <me@ikravets.com>
2015-11-23 17:27:02 +0200

    Only bug fixes in 2.3.6

--- platformio/__init__.py
+++ platformio/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-VERSION = (2, 4, "0.dev5")
+VERSION = (2, 3, "6.dev0")
 __version__ = ".".join([str(s) for s in VERSION])
 
 __title__ = "platformio"
