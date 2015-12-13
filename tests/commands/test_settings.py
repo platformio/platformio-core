4e90a2fd424eeb078957b779a211d9643c516566
Ivan Kravets <me@ikravets.com>
2015-12-14 01:14:02 +0200

    Fix test for settings command

--- tests/commands/test_settings.py
+++ tests/commands/test_settings.py
@@ -18,7 +18,8 @@ from platformio import app
 
 def test_settings_check(clirunner, validate_cliresult):
     result = clirunner.invoke(cli, ["get"])
-    validate_cliresult(result)
+    assert result.exit_code == 0
+    assert not result.exception
     assert len(result.output)
     for item in app.DEFAULT_SETTINGS.items():
         assert item[0] in result.output
