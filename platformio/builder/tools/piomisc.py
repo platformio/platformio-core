03c0033bedeb206116c7a7efc47bc8a62e6e8f07
Ivan Kravets <me@ikravets.com>
2016-04-30 17:33:45 +0300

    Optmize INO to CPP converter // Issue #639

--- platformio/builder/tools/piomisc.py
+++ platformio/builder/tools/piomisc.py
@@ -59,7 +59,8 @@ class InoToCPPConverter(object):
             return result
 
         prototype_names = set(
-            [p['match'].group(3).strip() for p in prototypes])
+            [p['match'].group(3).strip() for p in prototypes
+             if p['path'] == file_path])
         split_pos = prototypes[0]['match'].start()
         for item in prototypes:
             if item['path'] == file_path:
