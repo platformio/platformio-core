bc42fdbebed2d8195241cb563847a3a17cf5b251
Ivan Kravets <me@ikravets.com>
2016-11-21 16:28:37 +0200

    Remove obsolete code rated to LD_LIBRARY_PATH & Linux

--- platformio/pioplus.py
+++ platformio/pioplus.py
@@ -15,10 +15,9 @@
 import sys
 import os
 import subprocess
-from os.path import dirname, join
-from platform import system
+from os.path import join
 
-from platformio import app, exception, util
+from platformio import exception, util
 from platformio.managers.package import PackageManager
 
 PACKAGE_DEPS = {
@@ -67,8 +66,6 @@ def pioplus_call(args, **kwargs):
     pioplus_path = join(
         pm.get_package_dir(PACKAGE_DEPS['tool']['name'],
                            PACKAGE_DEPS['tool']['requirements']), "pioplus")
-    if system() == "Linux" and app.get_session_var("caller_id") == "Che":
-        os.environ['LD_LIBRARY_PATH'] = dirname(pioplus_path)
     os.environ['PYTHONEXEPATH'] = util.get_pythonexe_path()
     os.environ['PYTHONPYSITEDIR'] = pm.get_package_dir(
         PACKAGE_DEPS['pysite']['name'], PACKAGE_DEPS['pysite']['requirements'])
