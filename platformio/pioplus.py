1d4187162a94cd7732750e0f632664ea502deb3c
Ivan Kravets <me@ikravets.com>
2016-11-24 00:01:15 +0200

    Fix order for imports

--- platformio/pioplus.py
+++ platformio/pioplus.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
 import os
 import subprocess
+import sys
 from os.path import join
 
 from platformio import exception, util
