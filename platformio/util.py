c14f515526f125287f3c527e7e7d9504afab3bd8
Ivan Kravets <me@ikravets.com>
2016-12-05 15:48:59 +0200

    Propagate full PYTHONPATH

--- platformio/util.py
+++ platformio/util.py
@@ -359,8 +359,7 @@ def copy_pythonpath_to_osenv():
     if "PYTHONPATH" in os.environ:
         _PYTHONPATH = os.environ.get("PYTHONPATH").split(os.pathsep)
     for p in os.sys.path:
-        if p not in _PYTHONPATH and (isdir(join(p, "click")) or
-                                     isdir(join(p, "platformio"))):
+        if p not in _PYTHONPATH:
             _PYTHONPATH.append(p)
     os.environ['PYTHONPATH'] = os.pathsep.join(_PYTHONPATH)
 
