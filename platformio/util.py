fddcc3c965eeeb0fd86864f673561665b19702f6
Ivan Kravets <me@ikravets.com>
2017-06-14 19:41:36 +0300

    Fix SSL SNI issue for Python < 2.7.9

--- platformio/util.py
+++ platformio/util.py
@@ -477,14 +477,14 @@ def _get_api_result(
                 data=data,
                 headers=headers,
                 auth=auth,
-                verify=disable_ssl_check)
+                verify=not disable_ssl_check)
         else:
             r = _api_request_session().get(
                 url,
                 params=params,
                 headers=headers,
                 auth=auth,
-                verify=disable_ssl_check)
+                verify=not disable_ssl_check)
         result = r.json()
         r.raise_for_status()
     except requests.exceptions.HTTPError as e:
