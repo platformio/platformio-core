ca34da51aa807203ebab6672cf8ea2ddcaaabec2
Ivan Kravets <me@ikravets.com>
2018-03-22 17:37:51 +0200

    PIO Home: sort folders by name (ignore case), catch ServerError exceptions // Resolve #1454 Resolve #1302

--- platformio/managers/core.py
+++ platformio/managers/core.py
@@ -23,7 +23,7 @@ from platformio.managers.package import PackageManager
 CORE_PACKAGES = {
     "contrib-piohome": ">=0.9.1,<2",
     "contrib-pysite": ">=0.2.0,<2",
-    "tool-pioplus": ">=1.1.1,<2",
+    "tool-pioplus": ">=1.1.2,<2",
     "tool-unity": "~1.20302.1",
     "tool-scons": "~2.20501.4"
 }
