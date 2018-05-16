f7053928f0a7d3d65385e80e1f99b513364e4345
Ivan Kravets <me@ikravets.com>
2018-05-15 17:22:03 -0700

    Better explanation how to switch between stable/upstream dev/platforms

--- scripts/docspregen.py
+++ scripts/docspregen.py
@@ -302,7 +302,7 @@ Stable and upstream versions
 
 You can switch between `stable releases <https://github.com/platformio/platform-{name}/releases>`__
 of {title} development platform and the latest upstream version using
-:ref:`projectconf_env_platform` option as described below:
+:ref:`projectconf_env_platform` option in :ref:`projectconf` as described below:
 
 .. code-block:: ini
 
