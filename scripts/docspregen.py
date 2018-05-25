2f07a58e4f0d6a225b688c428f508d3be68ae1a5
Ivan Kravets <me@ikravets.com>
2018-05-25 16:17:02 +0300

    Split stable/upstream docs for dev/platforms

--- scripts/docspregen.py
+++ scripts/docspregen.py
@@ -302,21 +302,31 @@ Stable and upstream versions
 
 You can switch between `stable releases <https://github.com/platformio/platform-{name}/releases>`__
 of {title} development platform and the latest upstream version using
-:ref:`projectconf_env_platform` option in :ref:`projectconf` as described below:
+:ref:`projectconf_env_platform` option in :ref:`projectconf` as described below.
+
+Stable
+~~~~~~
 
 .. code-block:: ini
 
-    ; Custom stable version
-    [env:stable]
-    platform ={name}@x.y.z
+    ; Latest stable version
+    [env:latest_stable]
+    platform = {name}
     board = ...
-    ...
 
-    ; The latest upstream/development version
-    [env:upstream]
+    ; Custom stable version
+    [env:custom_stable]
+    platform = {name}@x.y.z
+    board = ...
+
+Upstream
+~~~~~~~~
+
+.. code-block:: ini
+
+    [env:upstream_develop]
     platform = https://github.com/platformio/platform-{name}.git
     board = ...
-    ...
 """.format(name=p.name, title=p.title))
 
     #
