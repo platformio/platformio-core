ff59dcefe0caca65ec770abb55f871b8b0c93aec
Ivan Kravets <me@ikravets.com>
2018-07-10 15:54:46 +0300

    Append __PLATFORMIO_DEBUG__ macro in debug session

--- platformio/builder/tools/piomisc.py
+++ platformio/builder/tools/piomisc.py
@@ -288,6 +288,7 @@ def PioClean(env, clean_dir):
 def ProcessDebug(env):
     if not env.subst("$PIODEBUGFLAGS"):
         env.Replace(PIODEBUGFLAGS=["-Og", "-g3", "-ggdb3"])
+    env.Append(PIODEBUGFLAGS=["-D__PLATFORMIO_DEBUG__"])
     env.Append(
         BUILD_FLAGS=env.get("PIODEBUGFLAGS", []),
         BUILD_UNFLAGS=["-Os", "-O0", "-O1", "-O2", "-O3"])
