519156512c774b20eb6f61a264ae7165e698a15f
Ivan Kravets <me@ikravets.com>
2019-08-28 23:01:39 +0300

    Strict versions for "semantic_version" and "tabulate"

--- setup.py
+++ setup.py
@@ -23,8 +23,8 @@ install_requires = [
     "colorama",
     "pyserial>=3,<4,!=3.3",
     "requests>=2.4.0,<3",
-    "semantic_version>=2.5.0,<3",
-    "tabulate>=0.8.3"
+    "semantic_version>=2.5.0,<2.7.0",
+    "tabulate>=0.8.3,<1"
 ]
 
 setup(
