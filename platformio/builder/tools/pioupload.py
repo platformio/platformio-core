7b6bab7f4e656d5b5c998cb680123b7bacca72a8
Ivan Kravets <me@ikravets.com>
2019-10-30 20:40:26 +0200

    Update memory usage banner

--- platformio/builder/tools/pioupload.py
+++ platformio/builder/tools/pioupload.py
@@ -249,7 +249,7 @@ def CheckUploadSize(_, target, source, env):
     program_size = _calculate_size(output, env.get("SIZEPROGREGEXP"))
     data_size = _calculate_size(output, env.get("SIZEDATAREGEXP"))
 
-    print ("Memory Usage -> http://bit.ly/pio-memory-usage")
+    print ('Advanced Memory Usage is available via "PlatformIO Home > Project Inspect"')
     if data_max_size and data_size > -1:
         print ("DATA:    %s" % _format_availale_bytes(data_size, data_max_size))
     if program_size > -1:
