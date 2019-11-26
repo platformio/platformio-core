742f60b48d5b6ca72351bf7d76b1598a76b5f47a
Ivan Kravets <me@ikravets.com>
2019-11-26 18:00:33 +0200

    Bump version to 4.1.1b4

--- platformio/managers/core.py
+++ platformio/managers/core.py
@@ -24,7 +24,7 @@ from platformio.proc import copy_pythonpath_to_osenv, get_pythonexe_path
 from platformio.project.config import ProjectConfig
 
 CORE_PACKAGES = {
-    "contrib-piohome": ">=3.1.0-beta.2,<3.2.0",
+    "contrib-piohome": ">=3.1.0-beta.3,<3.2.0",
     "contrib-pysite": "~2.%d%d.0" % (sys.version_info[0], sys.version_info[1]),
     "tool-pioplus": "^2.6.0",
     "tool-unity": "~1.20403.0",
