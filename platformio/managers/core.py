7d90c468ae8d7ebaba181eda06f240901ac9037a
Ivan Kravets <me@ikravets.com>
2019-12-22 01:27:51 +0200

    Updated SCons tool to 3.1.2

--- platformio/managers/core.py
+++ platformio/managers/core.py
@@ -28,7 +28,7 @@ CORE_PACKAGES = {
     "contrib-pysite": "~2.%d%d.0" % (sys.version_info[0], sys.version_info[1]),
     "tool-pioplus": "^2.6.1",
     "tool-unity": "~1.20403.0",
-    "tool-scons": "~2.20501.7" if PY2 else "~3.30101.0",
+    "tool-scons": "~2.20501.7" if PY2 else "~3.30102.0",
     "tool-cppcheck": "~1.189.0",
     "tool-clangtidy": "^1.80000.0",
 }
