178080fd12d573524d492e6c311e2868554610b2
Ivan Kravets <me@ikravets.com>
2020-01-04 13:53:08 +0200

    Improve boards search

--- platformio/commands/boards.py
+++ platformio/commands/boards.py
@@ -32,7 +32,10 @@ def cli(query, installed, json_output):  # pylint: disable=R0912
 
     grpboards = {}
     for board in _get_boards(installed):
-        if query and not any(query.lower() in board[k] for k in ("id", "name")):
+        if query and not any(
+            query.lower() in str(board.get(k, "")).lower()
+            for k in ("id", "name", "mcu", "vendor", "platform", "frameworks")
+        ):
             continue
         if board["platform"] not in grpboards:
             grpboards[board["platform"]] = []
