fbb62fa8a6b51467078312794ca35b1ca5f96db6
Ivan Kravets <me@ikravets.com>
2020-03-03 23:10:54 +0200

    Bump version to 4.2.2a3

--- platformio/__init__.py
+++ platformio/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-VERSION = (4, 2, "2a2")
+VERSION = (4, 2, "2a3")
 __version__ = ".".join([str(s) for s in VERSION])
 
 __title__ = "platformio"
