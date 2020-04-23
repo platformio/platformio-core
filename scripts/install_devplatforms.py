4d64968f7324db1bc64a68559ba963969d55d617
Ivan Kravets <me@ikravets.com>
2020-04-23 13:08:51 +0300

    Allow to ignore dev-platforms in Github action steps

--- scripts/install_devplatforms.py
+++ scripts/install_devplatforms.py
@@ -15,27 +15,26 @@
 import json
 import subprocess
 import sys
-from platformio import util
+
+import click
 
 
-def main():
+@click.command()
+@click.option("--desktop", is_flag=True, default=False)
+@click.option(
+    "--ignore",
+    envvar="PIO_INSTALL_DEVPLATFORMS_IGNORE",
+    help="Ignore names split by comma",
+)
+def main(desktop, ignore):
     platforms = json.loads(
         subprocess.check_output(
             ["platformio", "platform", "search", "--json-output"]
         ).decode()
     )
+    ignore = [n.strip() for n in (ignore or "").split(",") if n.strip()]
     for platform in platforms:
-        skip = [
-            platform["forDesktop"],
-            platform["name"] in ("ststm8", "infineonxmc", "microchippic32"),
-            # RISC-V GAP does not support Windows 86
-            util.get_systype() == "windows_x86" and platform["name"] == "riscv_gap",
-            # intel_mcs51: "version `CXXABI_1.3.9' not found (required by sdcc)"
-            # microchippic32: GCC does not work on x64
-            "linux" in util.get_systype()
-            and platform["name"] in ("aceinna_imu", "intel_mcs51"),
-            "darwin" in util.get_systype() and platform["name"] in ("gd32v", "nuclei"),
-        ]
+        skip = [not desktop and platform["forDesktop"], platform["name"] in ignore]
         if any(skip):
             continue
         subprocess.check_call(["platformio", "platform", "install", platform["name"]])
