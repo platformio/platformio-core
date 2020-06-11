1b6f94f5a51ca3f549ca7f9106d05dd8115331fd
Shahrustam <shahrustam98@gmail.com>
2020-06-11 13:28:38 +0300

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -80,7 +80,7 @@ def receive_email():  # pylint:disable=redefined-outer-name, too-many-locals
             _, mails = server.search(None, "ALL")
             for index in mails[0].split():
                 status, data = server.fetch(index, "(RFC822)")
-                if status != "OK":
+                if status != "OK" or not data or not isinstance(data[0], tuple):
                     continue
                 msg = email.message_from_string(
                     data[0][1].decode("ASCII", errors="surrogateescape")
