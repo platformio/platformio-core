ce7356794d9140045f3bb5b0db95886b7c8a61a2
Ivan Kravets <me@ikravets.com>
2020-12-26 21:43:41 +0200

    Test examples from the official dev-platforms

--- scripts/install_devplatforms.py
+++ scripts/install_devplatforms.py
@@ -26,15 +26,25 @@ import click
     envvar="PIO_INSTALL_DEVPLATFORMS_IGNORE",
     help="Ignore names split by comma",
 )
-def main(desktop, ignore):
+@click.option(
+    "--ownernames",
+    envvar="PIO_INSTALL_DEVPLATFORMS_OWNERNAMES",
+    help="Filter dev-platforms by ownernames (split by comma)",
+)
+def main(desktop, ignore, ownernames):
     platforms = json.loads(
         subprocess.check_output(
             ["platformio", "platform", "search", "--json-output"]
         ).decode()
     )
     ignore = [n.strip() for n in (ignore or "").split(",") if n.strip()]
+    ownernames = [n.strip() for n in (ownernames or "").split(",") if n.strip()]
     for platform in platforms:
-        skip = [not desktop and platform["forDesktop"], platform["name"] in ignore]
+        skip = [
+            not desktop and platform["forDesktop"],
+            platform["name"] in ignore,
+            ownernames and platform["ownername"] not in ownernames,
+        ]
         if any(skip):
             continue
         subprocess.check_call(["platformio", "platform", "install", platform["name"]])
