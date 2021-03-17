b54a8b40a4784047aae5925f911128762984cddf
Ivan Kravets <me@ikravets.com>
2021-03-17 17:42:11 +0200

    Refactor Unified Debugger to native Python Asynchronous I/O stack // Resolve #3793 , Resolve #3595

--- platformio/debug/process/server.py
+++ platformio/debug/process/server.py
@@ -12,53 +12,47 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import asyncio
 import os
 import time
-from os.path import isdir, isfile, join
-
-from twisted.internet import defer  # pylint: disable=import-error
-from twisted.internet import reactor  # pylint: disable=import-error
 
 from platformio import fs, util
-from platformio.commands.debug.exception import DebugInvalidOptionsError
-from platformio.commands.debug.helpers import escape_gdbmi_stream, is_gdbmi_mode
-from platformio.commands.debug.process.base import BaseProcess
+from platformio.debug.exception import DebugInvalidOptionsError
+from platformio.debug.helpers import escape_gdbmi_stream, is_gdbmi_mode
+from platformio.debug.process.base import DebugBaseProcess
 from platformio.proc import where_is_program
 
 
-class DebugServer(BaseProcess):
+class DebugServerProcess(DebugBaseProcess):
     def __init__(self, debug_options, env_options):
-        super(DebugServer, self).__init__()
+        super(DebugServerProcess, self).__init__()
         self.debug_options = debug_options
         self.env_options = env_options
 
         self._debug_port = ":3333"
-        self._transport = None
-        self._process_ended = False
         self._ready = False
 
-    @defer.inlineCallbacks
-    def spawn(self, patterns):  # pylint: disable=too-many-branches
+    async def run(self, patterns):  # pylint: disable=too-many-branches
         systype = util.get_systype()
         server = self.debug_options.get("server")
         if not server:
-            defer.returnValue(None)
+            return None
         server = self.apply_patterns(server, patterns)
         server_executable = server["executable"]
         if not server_executable:
-            defer.returnValue(None)
+            return None
         if server["cwd"]:
-            server_executable = join(server["cwd"], server_executable)
+            server_executable = os.path.join(server["cwd"], server_executable)
         if (
             "windows" in systype
             and not server_executable.endswith(".exe")
-            and isfile(server_executable + ".exe")
+            and os.path.isfile(server_executable + ".exe")
         ):
             server_executable = server_executable + ".exe"
 
-        if not isfile(server_executable):
+        if not os.path.isfile(server_executable):
             server_executable = where_is_program(server_executable)
-        if not isfile(server_executable):
+        if not os.path.isfile(server_executable):
             raise DebugInvalidOptionsError(
                 "\nCould not launch Debug Server '%s'. Please check that it "
                 "is installed and is included in a system PATH\n\n"
@@ -70,6 +64,7 @@ class DebugServer(BaseProcess):
         openocd_pipe_allowed = all(
             [not self.debug_options["port"], "openocd" in server_executable]
         )
+        openocd_pipe_allowed = False
         if openocd_pipe_allowed:
             args = []
             if server["cwd"]:
@@ -83,34 +78,31 @@ class DebugServer(BaseProcess):
             )
             self._debug_port = '| "%s" %s' % (server_executable, str_args)
             self._debug_port = fs.to_unix_path(self._debug_port)
-            defer.returnValue(self._debug_port)
+            return self._debug_port
 
         env = os.environ.copy()
         # prepend server "lib" folder to LD path
         if (
             "windows" not in systype
             and server["cwd"]
-            and isdir(join(server["cwd"], "lib"))
+            and os.path.isdir(os.path.join(server["cwd"], "lib"))
         ):
             ld_key = "DYLD_LIBRARY_PATH" if "darwin" in systype else "LD_LIBRARY_PATH"
-            env[ld_key] = join(server["cwd"], "lib")
+            env[ld_key] = os.path.join(server["cwd"], "lib")
             if os.environ.get(ld_key):
                 env[ld_key] = "%s:%s" % (env[ld_key], os.environ.get(ld_key))
         # prepend BIN to PATH
-        if server["cwd"] and isdir(join(server["cwd"], "bin")):
+        if server["cwd"] and os.path.isdir(os.path.join(server["cwd"], "bin")):
             env["PATH"] = "%s%s%s" % (
-                join(server["cwd"], "bin"),
+                os.path.join(server["cwd"], "bin"),
                 os.pathsep,
                 os.environ.get("PATH", os.environ.get("Path", "")),
             )
 
-        self._transport = reactor.spawnProcess(
-            self,
-            server_executable,
-            [server_executable] + server["arguments"],
-            path=server["cwd"],
-            env=env,
+        await self.spawn(
+            *([server_executable] + server["arguments"]), cwd=server["cwd"], env=env
         )
+
         if "mspdebug" in server_executable.lower():
             self._debug_port = ":2000"
         elif "jlink" in server_executable.lower():
@@ -118,19 +110,18 @@ class DebugServer(BaseProcess):
         elif "qemu" in server_executable.lower():
             self._debug_port = ":1234"
 
-        yield self._wait_until_ready()
+        await self._wait_until_ready()
 
-        defer.returnValue(self._debug_port)
+        return self._debug_port
 
-    @defer.inlineCallbacks
-    def _wait_until_ready(self):
+    async def _wait_until_ready(self):
         ready_pattern = self.debug_options.get("server", {}).get("ready_pattern")
         timeout = 60 if ready_pattern else 10
         elapsed = 0
         delay = 0.5
         auto_ready_delay = 0.5
-        while not self._ready and not self._process_ended and elapsed < timeout:
-            yield self.async_sleep(delay)
+        while not self._ready and self.is_running() and elapsed < timeout:
+            await asyncio.sleep(delay)
             if not ready_pattern:
                 self._ready = self._last_activity < (time.time() - auto_ready_delay)
             elapsed += delay
@@ -143,33 +134,15 @@ class DebugServer(BaseProcess):
             self._ready = ready_pattern.encode() in data
         return self._ready
 
-    @staticmethod
-    def async_sleep(secs):
-        d = defer.Deferred()
-        reactor.callLater(secs, d.callback, None)
-        return d
-
     def get_debug_port(self):
         return self._debug_port
 
-    def outReceived(self, data):
-        super(DebugServer, self).outReceived(
+    def stdout_data_received(self, data):
+        super(DebugServerProcess, self).stdout_data_received(
             escape_gdbmi_stream("@", data) if is_gdbmi_mode() else data
         )
         self._check_ready_by_pattern(data)
 
-    def errReceived(self, data):
-        super(DebugServer, self).errReceived(data)
+    def stderr_data_received(self, data):
+        super(DebugServerProcess, self).stderr_data_received(data)
         self._check_ready_by_pattern(data)
-
-    def processEnded(self, reason):
-        self._process_ended = True
-        super(DebugServer, self).processEnded(reason)
-
-    def terminate(self):
-        if self._process_ended or not self._transport:
-            return
-        try:
-            self._transport.signalProcess("KILL")
-        except:  # pylint: disable=bare-except
-            pass
