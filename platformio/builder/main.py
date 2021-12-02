4f4c88aca93699bad0cebe4a13e4f7aa1b6d5fcd
Ivan Kravets <me@ikravets.com>
2021-12-02 22:16:37 +0200

    Use SCons vars for deprecated variables

--- platformio/builder/main.py
+++ platformio/builder/main.py
@@ -115,10 +115,10 @@ env.Replace(
     PROJECT_LIBDEPS_DIR=config.get("platformio", "libdeps_dir"),
     PROJECT_INCLUDE_DIR=config.get("platformio", "include_dir"),
     PROJECT_SRC_DIR=config.get("platformio", "src_dir"),
-    PROJECTSRC_DIR=config.get("platformio", "src_dir"),  # legacy for dev/platform
+    PROJECTSRC_DIR="PROJECT_SRC_DIR",  # legacy for dev/platform
     PROJECT_TEST_DIR=config.get("platformio", "test_dir"),
     PROJECT_DATA_DIR=config.get("platformio", "data_dir"),
-    PROJECTDATA_DIR=config.get("platformio", "data_dir"),  # legacy for dev/platform
+    PROJECTDATA_DIR="$PROJECT_DATA_DIR",  # legacy for dev/platform
     PROJECT_BUILD_DIR=config.get("platformio", "build_dir"),
     BUILD_CACHE_DIR=config.get("platformio", "build_cache_dir"),
     LIBSOURCE_DIRS=[
