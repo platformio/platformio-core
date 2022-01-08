5658e7f71888a29a268b5c71af6019377b048b9b
Alexey Vazhnov <vazhnov@boot-keys.org>
2022-01-08 15:59:47 +0300

    _internet_on: try IPv4, if not acceptable — try IPv6 (#4151)
    * _internet_on: try IPv4, if not acceptable — try IPv6
    
    * _internet_on: replace IPv4 `socket.socket` + IPv6 `socket.socket` with one universal `socket.create_connection`

--- platformio/clients/http.py
+++ platformio/clients/http.py
@@ -196,8 +196,9 @@ def _internet_on():
                     continue
                 requests.get("http://%s" % host, allow_redirects=False, timeout=timeout)
                 return True
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((host, 80))
+            # try to resolve `host` for both AF_INET and AF_INET6, and then try to connect
+            # to all possible addresses (IPv4 and IPv6) in turn until a connection succeeds:
+            s = socket.create_connection((host, 80))
             s.close()
             return True
         except:  # pylint: disable=bare-except
