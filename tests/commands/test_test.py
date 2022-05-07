5b00f6fb95ee1beea8cce3390535013ac2155485
Ivan Kravets <me@ikravets.com>
2022-05-07 17:55:32 +0300

    Skip "test_doctest_framework" from Github Actions / Windows

--- tests/commands/test_test.py
+++ tests/commands/test_test.py
@@ -436,6 +436,10 @@ void unittest_uart_end(){}
     validate_cliresult(result)
 
 
+@pytest.mark.skipif(
+    sys.platform == "win32" and os.environ.get("GITHUB_ACTIONS"),
+    reason="skip Github Actions on Windows (MinGW issue)",
+)
 def test_doctest_framework(clirunner, tmp_path: Path):
     project_dir = tmp_path / "project"
     project_dir.mkdir()
