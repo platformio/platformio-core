45fcb40a5c06d400cb15af9940b6eb407a88f32d
Ivan Kravets <me@ikravets.com>
2022-05-18 16:16:51 +0300

    Dynamically load refactored dev-platform module

--- platformio/platform/factory.py
+++ platformio/platform/factory.py
@@ -14,11 +14,12 @@
 
 import os
 import re
+import sys
 
 from platformio import fs
 from platformio.compat import load_python_module
 from platformio.package.meta import PackageItem
-from platformio.platform.base import PlatformBase
+from platformio.platform import base
 from platformio.platform.exception import UnknownPlatform
 
 
@@ -28,15 +29,18 @@ class PlatformFactory(object):
         name = re.sub(r"[^\da-z\_]+", "", name, flags=re.I)
         return "%sPlatform" % name.lower().capitalize()
 
-    @staticmethod
-    def load_module(name, path):
+    @classmethod
+    def load_platform_module(cls, name, path, by_fallback=False):
         try:
             return load_python_module("platformio.platform.%s" % name, path)
-        except ImportError:
+        except ImportError as exc:
+            if "platformio.managers" in str(exc) and not by_fallback:
+                sys.modules["platformio.managers.platform"] = base
+                return cls.load_platform_module(name, path, by_fallback=True)
             raise UnknownPlatform(name)
 
     @classmethod
-    def new(cls, pkg_or_spec, autoinstall=False) -> PlatformBase:
+    def new(cls, pkg_or_spec, autoinstall=False) -> base.PlatformBase:
         # pylint: disable=import-outside-toplevel
         from platformio.package.manager.platform import PlatformPackageManager
 
@@ -72,16 +76,16 @@ class PlatformFactory(object):
         platform_cls = None
         if os.path.isfile(os.path.join(platform_dir, "platform.py")):
             platform_cls = getattr(
-                cls.load_module(
+                cls.load_platform_module(
                     platform_name, os.path.join(platform_dir, "platform.py")
                 ),
                 cls.get_clsname(platform_name),
             )
         else:
             platform_cls = type(
-                str(cls.get_clsname(platform_name)), (PlatformBase,), {}
+                str(cls.get_clsname(platform_name)), (base.PlatformBase,), {}
             )
 
         _instance = platform_cls(os.path.join(platform_dir, "platform.json"))
-        assert isinstance(_instance, PlatformBase)
+        assert isinstance(_instance, base.PlatformBase)
         return _instance
