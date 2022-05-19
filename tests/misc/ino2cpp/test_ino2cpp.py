f4d976945089ec55c1654534b105e51bb2c7cfc0
Ivan Kravets <me@ikravets.com>
2022-05-19 14:36:24 +0300

    Move ino2cpp tests to the misc folder

--- tests/misc/ino2cpp/test_ino2cpp.py
+++ tests/misc/ino2cpp/test_ino2cpp.py
@@ -17,16 +17,16 @@ from os.path import dirname, isdir, join, normpath
 
 from platformio.commands.ci import cli as cmd_ci
 
-INOTEST_DIR = normpath(join(dirname(__file__), "ino2cpp"))
+EXAMPLES_DIR = normpath(join(dirname(__file__), "examples"))
 
 
 def pytest_generate_tests(metafunc):
     if "piotest_dir" not in metafunc.fixturenames:
         return
     test_dirs = []
-    for name in listdir(INOTEST_DIR):
-        if isdir(join(INOTEST_DIR, name)):
-            test_dirs.append(join(INOTEST_DIR, name))
+    for name in listdir(EXAMPLES_DIR):
+        if isdir(join(EXAMPLES_DIR, name)):
+            test_dirs.append(join(EXAMPLES_DIR, name))
     test_dirs.sort()
     metafunc.parametrize("piotest_dir", test_dirs)
 
@@ -37,10 +37,10 @@ def test_example(clirunner, validate_cliresult, piotest_dir):
 
 
 def test_warning_line(clirunner, validate_cliresult):
-    result = clirunner.invoke(cmd_ci, [join(INOTEST_DIR, "basic"), "-b", "uno"])
+    result = clirunner.invoke(cmd_ci, [join(EXAMPLES_DIR, "basic"), "-b", "uno"])
     validate_cliresult(result)
     assert 'basic.ino:16:14: warning: #warning "Line number is 16"' in result.output
     assert 'basic.ino:46:2: warning: #warning "Line number is 46"' in result.output
-    result = clirunner.invoke(cmd_ci, [join(INOTEST_DIR, "strmultilines"), "-b", "uno"])
+    result = clirunner.invoke(cmd_ci, [join(EXAMPLES_DIR, "strmultilines"), "-b", "uno"])
     validate_cliresult(result)
     assert 'main.ino:75:2: warning: #warning "Line 75"' in result.output
