11a43b26930c87c4b853f4dde756f7217d838c51
Ivan Kravets <me@ikravets.com>
2022-05-31 17:31:21 +0300

    Regroup "system" commands

--- platformio/system/commands/__init__.py
+++ platformio/system/commands/__init__.py
@@ -11,22 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-import click
-
-from platformio.system.commands.completion import system_completion_cmd
-from platformio.system.commands.info import system_info_cmd
-from platformio.system.commands.prune import system_prune_cmd
-
-
-@click.group(
-    "system",
-    commands=[
-        system_completion_cmd,
-        system_info_cmd,
-        system_prune_cmd,
-    ],
-    short_help="Miscellaneous system commands",
-)
-def cli():
-    pass
