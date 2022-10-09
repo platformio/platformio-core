65d59f1e823f72f5d09c42c38b5e6df1fe854dc9
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-09 18:26:59 +0000

    Adding tarfile member sanitization to extractall()

--- platformio/remote/projectsync.py
+++ platformio/remote/projectsync.py
@@ -113,5 +113,24 @@ class ProjectSync:
     def decompress_items(self, fileobj):
         fileobj.seek(0)
         with tarfile.open(fileobj=fileobj, mode="r:gz") as tgz:
-            tgz.extractall(self.path)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tgz, self.path)
         return True
