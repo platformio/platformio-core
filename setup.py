5b13aeda5261f0048bbe9b257f5533708c3e713a
Ivan Kravets <me@ikravets.com>
2023-02-18 13:48:20 +0200

    Update deps

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ minimal_requirements = [
 home_requirements = [
     "aiofiles==%s" % ("0.8.0" if PY36 else "23.1.*"),
     "ajsonrpc==1.*",
-    "starlette==%s" % ("0.19.1" if PY36 else "0.24.*"),
+    "starlette==%s" % ("0.19.1" if PY36 else "0.25.*"),
     "uvicorn==%s" % ("0.16.0" if PY36 else "0.20.*"),
     "wsproto==%s" % ("1.0.0" if PY36 else "1.2.*"),
 ]
