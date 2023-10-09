2458309d553d2a5bdf99be16dc8f386a7d042fdc
Ivan Kravets <me@ikravets.com>
2023-10-09 11:41:01 +0300

    Update SPDX to 3.22

--- platformio/package/manifest/schema.py
+++ platformio/package/manifest/schema.py
@@ -276,7 +276,7 @@ class ManifestSchema(BaseSchema):
     @staticmethod
     @memoized(expire="1h")
     def load_spdx_licenses():
-        version = "3.21"
+        version = "3.22"
         spdx_data_url = (
             "https://raw.githubusercontent.com/spdx/license-list-data/"
             f"v{version}/json/licenses.json"
