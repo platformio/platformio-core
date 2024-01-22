84c7ede0e102857c0edb852c0572514aa637883b
Ivan Kravets <me@ikravets.com>
2024-01-22 22:54:58 +0200

    Sync docs

--- platformio/pipdeps.py
+++ platformio/pipdeps.py
@@ -35,7 +35,7 @@ def get_pip_dependencies():
     home = [
         # PIO Home requirements
         "ajsonrpc == 1.2.*",
-        "starlette >=0.19, <0.37",
+        "starlette >=0.19, <0.36",
         "uvicorn %s" % ("== 0.16.0" if PY36 else ">=0.16, <0.28"),
         "wsproto == 1.*",
     ]
