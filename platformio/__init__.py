aea9075d4bd1ea3c2b035c83e8a0c114dce79514
Ivan Kravets <me@ikravets.com>
2024-07-29 15:33:33 +0300

    Bump version to 6.1.16b1

--- platformio/__init__.py
+++ platformio/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-VERSION = (6, 1, "16a1")
+VERSION = (6, 1, "16b1")
 __version__ = ".".join([str(s) for s in VERSION])
 
 __title__ = "platformio"
